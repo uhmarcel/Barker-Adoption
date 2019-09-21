36511975b0243a7e2e3aea8fe1c968170ea9f741
Marcel Riera <mrier011@fiu.edu>
2019-09-21 19:23:38 -0400

    Added dogCard component

---
+++ barker/src/Components/Main/DogCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class DogCard extends Component {
+
+    render() {
+        return (
+            <div>
+                Whatsup
+            </div>
+        );
+    }
+
+}
