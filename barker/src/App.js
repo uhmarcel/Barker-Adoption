97fbc560807bde4a31afd9be62d5e4f59f7eb920
ettore34 <ettoremottolalares@gmail.com>
2019-09-21 20:12:35 -0400

    added matches

--- barker/src/App.js
+++ barker/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import DogCard from './Components/Main/DogCard';
+import Matches from './Components/Main/Matches';
 import './App.css';
 
 function App() {
   
   return (
     <div className="App">
-        <DogCard />
+        <DogCard/>
     </div>
   );
 }
